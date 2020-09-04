c286b91f1b83d9c3a570710e81d04b723279a7ed
Vinay <vinay2019eu@gmail.com>
2020-09-04 11:47:39 +0400

    changes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode=require('./util/geocode')
 const forcast=require('./util/forcast')
 
-
 const app = express()
+const port = process.env.PORT || 3000
 //define path
 const viewsPath=path.join(__dirname, '../templates/views')
 const partialsPath=path.join(__dirname, '../templates/partials')
@@ -101,6 +101,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port ' +port)
 })
